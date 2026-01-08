4765fafd4e2966d5d930ee1d3f10d6f07a80de61
callmeSHREYAS <shreyasvartak83@gmail.com>
2026-01-08 17:42:05 +0530

    Arcjet installed

--- controller/authController.js
+++ controller/authController.js
@@ -19,12 +19,12 @@ const handelLogin = async (req, res) => {
         const accessToken = jwt.sign(
             {'username':foundUser.username},
             process.env.ACCESS_TOKEN_SECRET,
-            {expiresIn:'1d'}
+            {expiresIn:'30s'}
         )
         const refreshToken = jwt.sign(
             {'username':foundUser.username},
             process.env.REFRESH_TOKEN_SECRET,
-            {expiresIn:'4d'}
+            {expiresIn:'1d'}
         )
         res.cookie('jwt', refreshToken, {
             httpOnly: true,
--- middleware/verifyJWT.js
+++ middleware/verifyJWT.js
@@ -1,14 +1,14 @@
 const jwt = require('jsonwebtoken')
 
 
-const verifyJWT = async(req,res,next)=>{
-    const authHeader = req.headers.authorization
-    if(!authHeader?.startsWith('Bearer ')){
-        return res.sendStatus(401)
-    }
+const verifyJWT = (req,res,next)=>{
+    const authHeader = req.headers['authorization']
+    if(!authHeader?.startsWith('Bearer '))return res.sendStatus(401)
+    
     const token = authHeader.split(' ')[1]
     jwt.verify(
         token,
+        process.env.ACCESS_TOKEN_SECRET,
         (err,decoded)=>{
             if(err)return res.sendStatus(403);
             req.user=decoded.username;
@@ -17,3 +17,25 @@ const verifyJWT = async(req,res,next)=>{
     )
 }
 module.exports = verifyJWT
+// const jwt = require('jsonwebtoken')
+
+
+// const verifyJWT = (req, res, next) => {
+//     const authHeader = req.headers['authorization']
+//     if (!authHeader?.startsWith('Bearer ')) return res.sendStatus(401);
+//     console.log(authHeader);
+//     const token = authHeader.split(' ')[1]
+//     jwt.verify(
+//         token,
+//         process.env.ACCESS_TOKEN_SECRET,
+//         (err, decoded) => {
+//             if (err) return res.sendStatus(403) // invalid token
+//             req.user = decoded.username;
+            
+//             next()
+//         }
+//     )
+// }
+
+
+// module.exports = verifyJWT  
